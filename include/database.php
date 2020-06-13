f6efd4e6371fd4d4de07beec0356f3e9b528db8b
Miglė Zubavičiūtė <miglezub@gmail.com>
2020-06-13 19:20:59 +0300

    changed comment list query

--- include/database.php
+++ include/database.php
@@ -29,22 +29,50 @@ class Database {
     }
     //gets comment and subcomment list
     function getComments() {
-        //selects all comments
-        $query = "SELECT * FROM comments ORDER BY date DESC";
+        //Right join is used to select all comments even if they don't have subcomments
+        //returns an ordered list where comments having more than 1 subcomment appear more than once
+        $query = "SELECT 
+            subcomments.id AS sub_id, 
+            subcomments.name AS sub_name, 
+            subcomments.email AS sub_email, 
+            subcomments.comment AS sub_comment,
+            subcomments.date as sub_date, 
+            fk_comment, 
+            comments.* 
+            FROM subcomments
+            RIGHT JOIN comments ON subcomments.fk_comment=comments.id
+            ORDER BY comments.date DESC, subcomments.date DESC";
         $result = mysqli_query($this->connection, $query);
 
         $comments = array();
-        //goes through the list of comments, gets subcomments for each comment
+        $previousRow = null;
         while($row = mysqli_fetch_assoc($result)) {
-            $query = "SELECT * FROM subcomments WHERE fk_comment = '" . $row['id'] . "' ORDER BY date DESC";
-            $subcom = mysqli_query($this->connection, $query);
-            //goes through all subcomments, inserts subcomment data to array
-            while($subRow = mysqli_fetch_assoc($subcom)) {
-                $row['subcomments'][] = $subRow;
+            //if it is the first row or comment appears for the first time in a list
+            if($previousRow['id'] != $row['id'] || $previousRow == null){
+                $newRow = $this->makeRow($row['id'], $row['name'], $row['comment'], $row['date']);
+                if($row['sub_id'] != NULL) {
+                    $newRow['subcomments'][] = 
+                        $this->makeRow($row['sub_id'], $row['sub_name'], $row['sub_comment'], $row['sub_date']);
+                }
+                $comments[] = $newRow;
             }
-            $comments[] = $row;
+            //if previous row had the same comment it means that only subcomment must be added
+            else {
+                $comments[count($comments) - 1]['subcomments'][] = 
+                    $this->makeRow($row['sub_id'], $row['sub_name'], $row['sub_comment'], $row['sub_date']);
+            }
+            $previousRow = $row;
         }
         return $comments;
+    }
+
+    function makeRow($id, $name, $comment, $date) {
+        $newRow = null;
+        $newRow['id'] = $id;
+        $newRow['name'] = $name;
+        $newRow['comment'] = $comment;
+        $newRow['date'] = $date;
+        return $newRow;
     }
 }
 
